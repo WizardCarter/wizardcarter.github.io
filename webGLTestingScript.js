89a7419d61b037afdaf97139e321ba926ca5ebd4
Carter <carterpryor2003@gmail.com>
2017-01-17 21:20:57 -0500

    Update webGLTestingScript.js
    Initialize WebGL context and clear buffer bits.

--- webGLTestingScript.js
+++ webGLTestingScript.js
@@ -1,1 +1,32 @@
+var gl;
 
+function initWebGL(var canvas) {
+  gl = null;
+  
+  gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+  
+  if (!gl) {
+    alert("The WebGL context failed to initialize. You may need to update your browser.");  
+  }
+  
+  return gl;
+}
+
+function start() {
+  var canvas = document.getElementById("paint");
+  
+  gl = initWebGL(canvas);
+  
+  if (!gl) {
+    document.write("The WebGL context failed to initialize.");
+    return;
+  }
+  
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  
+  /* FOR 3D
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LEQUAL);
+  */
+  gl.clear(gl.COLOR_BUFFER_BIT /* | gl.DEPTH_BUFFER_BIT*/);
+}

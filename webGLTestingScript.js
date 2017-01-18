b77fea065789304cab8b2b813a0d97eef56d79df
Carter <carterpryor2003@gmail.com>
2017-01-17 21:36:13 -0500

    Update webGLTestingScript.js
    Added a function to compile shaders for me.

--- webGLTestingScript.js
+++ webGLTestingScript.js
@@ -3,9 +3,9 @@ var gl;
 function initWebGL(var canvas) {
   gl = null;
   
-  gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+  gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl"); //get the context
   
-  if (!gl) {
+  if (!gl) { //if it didn't initialize
     alert("The WebGL context failed to initialize. You may need to update your browser.");  
   }
   
@@ -13,7 +13,7 @@ function initWebGL(var canvas) {
 }
 
 function start() {
-  var canvas = document.getElementById("paint");
+  var canvas = document.getElementById("paint"); //get the canvas element
   
   gl = initWebGL(canvas);
   
@@ -22,11 +22,33 @@ function start() {
     return;
   }
   
-  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0); //clear colors
   
   /* FOR 3D
   gl.enable(gl.DEPTH_TEST);
   gl.depthFunc(gl.LEQUAL);
   */
   gl.clear(gl.COLOR_BUFFER_BIT /* | gl.DEPTH_BUFFER_BIT*/);
+}
+
+function makeShader(var type, var source) {
+  var shader;
+  if (type == "vertex") { //set shader type
+    shader = gl.createShader(gl.VERTEX_SHADER);  
+  } else if (type == "fragment") {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);  
+  } else if (type == "geometry") {
+    shader = gl.createShader(gl.GEOMETRY_SHADER);  
+  }
+  
+  gl.shaderSource(shader, source); //set it's source
+  gl.compileShader(shader); //and compile it
+  
+  //get any compile errors
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {  
+      console.log("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));  
+      gl.deleteShader(shader);
+      return null;  
+  }
+  return shader; //and return the shader
 }
